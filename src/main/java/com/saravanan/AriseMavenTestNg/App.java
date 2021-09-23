764567d7c9d8a3571d105d4692a0df96d36af1e8
samsaravananr <samsaravananr@gmail.com>
2021-09-23 15:50:37 +0530

    First Commit

---
+++ src/main/java/com/saravanan/AriseMavenTestNg/App.java
@@ -0,0 +1,13 @@
+package com.saravanan.AriseMavenTestNg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
